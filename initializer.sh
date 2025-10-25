e445841b6df0286df28e4d30fedf9449fd4c0d04
Hossein Radmard <iranradmard1@gmail.com>
2025-10-25 08:30:02 +0330

    add hints in initializer.sh and cleanuo ansible.cfg

--- initializer.sh
+++ initializer.sh
@@ -5,3 +5,12 @@ sudo add-apt-repository --yes --update ppa:ansible/ansible || true
 sudo apt install -y ansible
 echo "[SUCCESS] Ansible installed via APT."
 ansible --version
+
+
+# echo "[INFO] Installing Ansible via Python..."
+# python3 -m venv venv
+# echo "[INFO] Please source the python virtual environment."
+# # source venv/bin/activate
+## ---
+# pip3 install -r requirements.txt
+## ansible --version
